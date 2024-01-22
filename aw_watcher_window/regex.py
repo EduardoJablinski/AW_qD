3b5fadb0564b44a21c393c9af50c57a2ae1bfff8
EduardoJablinski <rm550975@fiap.com.br>
2024-01-22 10:09:02 -0300

    último regex

--- aw_watcher_window/regex.py
+++ aw_watcher_window/regex.py
@@ -1,7 +1,10 @@
 import re
 
 from .config import load_config
+from collections import deque
 
+assign_last_value = True
+lista_regex = deque(maxlen=3)
 
 def carregar_regras():
     config = load_config()
@@ -12,17 +15,29 @@ def carregar_regras():
     return regex_toml
 
 def aplicar_regex(texto, regras):
+    global assign_last_value
     resultados = []
+
     for regra in regras:
         nome = regra["nome"]
         padrao = regra["padrao"]
         correspondencias = re.findall(padrao, texto)
         if correspondencias:
             resultados.append(nome)
-    return ', '.join(resultados) if resultados else "Regex não encontrado"
+
+    if resultados:
+        assign_last_value = True    
+        return ', '.join(resultados)
+    elif assign_last_value and lista_regex:
+        assign_last_value = False
+        return lista_regex[-1]
+    else:
+        return "Regex não encontrado"
 
 def regex(texto):
     regras = carregar_regras()
     regex_result = aplicar_regex(texto, regras)
     #print(f"Título: {texto}, Resultado: {regex_result}")
+    lista_regex.append(regex_result)
+    #print(f"Lista: {teste}")
     return regex_result
